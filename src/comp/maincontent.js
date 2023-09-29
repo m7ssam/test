7a8e2fe5f541bfba8d9864976d9ea7a2e4f1abe6
m7ossam <mohamed7ossam@outlook.com>
2023-09-29 11:21:27 +0300

    fix

--- src/comp/maincontent.js
+++ src/comp/maincontent.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Bodycontent from "../comp/bodycontent";
 import "./css/main.css";
-import styles from "./css/color.css";
 
 const Maincontent = ({ pageName, designer }) => {
   return (
     <main>
-      <h3 className={styles.mainColor}>{pageName} Page</h3>
+      <h3>{pageName} Page</h3>
       {designer != null && <h4>designed by {designer}</h4>}
       <hr />
       <Bodycontent />
